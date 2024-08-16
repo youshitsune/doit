1c86fd7652c6afb3c9e3f05b540c419af48a06f9
youshitsune <youshitsune@dmz.rs>
2024-08-16 11:28:32 +0200

    refactor: change way of saving tasks and add auth to list function

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 
 	"github.com/boltdb/bolt"
@@ -15,12 +16,19 @@ const bucket = "tasks"
 
 var db, _ = bolt.Open("db", 0600, nil)
 
-func put(key string, value string) {
+func setupdb() {
+	db.Update(func(tx *bolt.Tx) error {
+		b, err := tx.CreateBucket([]byte(bucket))
+		if err == nil {
+			b.Put([]byte("l"), []byte("0"))
+		}
+		return nil
+	})
+}
+
+func put(key, value string) {
 	db.Update(func(tx *bolt.Tx) error {
 		b := tx.Bucket([]byte(bucket))
-		if b == nil {
-			b, _ = tx.CreateBucket([]byte(bucket))
-		}
 		b.Put([]byte(key), []byte(value))
 		return nil
 	})
@@ -37,14 +45,14 @@ func get(key string) []byte {
 	return r
 }
 
-func getall() [][]string {
-	r := make([][]string, 0)
+func getall() []string {
+	r := make([]string, 0)
 
 	db.View(func(tx *bolt.Tx) error {
 		b := tx.Bucket([]byte(bucket))
 
 		b.ForEach(func(k, v []byte) error {
-			r = append(r, []string{string(k), string(v)})
+			r = append(r, string(v))
 			return nil
 		})
 
@@ -54,8 +62,40 @@ func getall() [][]string {
 	return r
 }
 
+func gettasks() []string {
+	r := make([]string, 0)
+
+	db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(bucket))
+
+		b.ForEach(func(k, v []byte) error {
+			if string(k) != "l" {
+				r = append(r, string(v))
+			}
+			return nil
+		})
+
+		return nil
+	})
+
+	return r
+}
+
+func addtask(value string) {
+	db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(bucket))
+		tkey, _ := strconv.Atoi(string(b.Get([]byte("l"))))
+		tkey++
+		key := strconv.Itoa(tkey)
+		b.Put([]byte(key), []byte(value+"``0"))
+		b.Put([]byte("l"), []byte(key))
+		return nil
+	})
+}
+
 func main() {
 	defer db.Close()
+	setupdb()
 
 	data, err := os.ReadFile("auth")
 	if err != nil {
@@ -70,26 +110,32 @@ func main() {
 		user := c.FormValue("user")
 		password := c.FormValue("password")
 		if user == strings.Trim(auth[0], "\n") && password == strings.Trim(auth[1], "\n") {
-			put(task, "0")
+			addtask(task)
 			return c.String(http.StatusOK, "Success!\n")
 		}
 		return c.String(http.StatusOK, "Failed!\n")
 	})
 
 	e.POST("/list", func(c echo.Context) error {
+		user := c.FormValue("user")
+		password := c.FormValue("password")
 		res := "Tasks:\n"
-		r := getall()
-		for i := range r {
-			var status string
-			if r[i][1] == "0" {
-				status = "Not finished"
-			} else {
-				status = "Finished"
+		r := make([]string, 0)
+		if user == strings.Trim(auth[0], "\n") && password == strings.Trim(auth[1], "\n") {
+			r = gettasks()
+			for i := range r {
+				t := strings.Split(r[i], "``")
+				var status string
+				if t[1] == "0" {
+					status = "Not finished"
+				} else {
+					status = "Finished"
+				}
+				res += fmt.Sprintf("%v ----- %v\n", t[0], status)
 			}
-			res += fmt.Sprintf("%v ----- %v\n", r[i][0], status)
+			return c.String(http.StatusOK, res)
 		}
-		return c.String(http.StatusOK, res)
+		return c.String(http.StatusOK, "Failed!\n")
 	})
-
 	e.Logger.Fatal(e.Start(":3333"))
 }
