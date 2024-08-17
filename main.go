33c92553390630a2aedd6d0565a49fee79d02448
youshitsune <youshitsune@dmz.rs>
2024-08-17 15:40:20 +0200

    refactor: switch the database to nosql

--- main.go
+++ main.go
@@ -8,89 +8,65 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/boltdb/bolt"
+	"math/rand"
+
 	"github.com/labstack/echo/v4"
+	c "github.com/ostafen/clover/v2"
+	d "github.com/ostafen/clover/v2/document"
+	q "github.com/ostafen/clover/v2/query"
 )
 
 const bucket = "tasks"
 
-var db, _ = bolt.Open("db", 0600, nil)
+var db, _ = c.Open("")
+
+var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func random() string {
+	b := make([]rune, 10)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
 
 func setupdb() {
-	db.Update(func(tx *bolt.Tx) error {
-		b, err := tx.CreateBucket([]byte(bucket))
-		if err == nil {
-			b.Put([]byte("l"), []byte("-1"))
-		}
-		return nil
-	})
+	truthy, _ := db.HasCollection(bucket)
+	if !truthy {
+		db.CreateCollection(bucket)
+	}
 }
 
-func put(key, value string) {
-	db.Update(func(tx *bolt.Tx) error {
-		b := tx.Bucket([]byte(bucket))
-		b.Put([]byte(key), []byte(value))
-		return nil
-	})
-}
-
-func get(key string) []byte {
-	r := make([]byte, 0)
-	db.View(func(tx *bolt.Tx) error {
-		b := tx.Bucket([]byte(bucket))
-		r = b.Get([]byte(key))
-		return nil
-	})
-
-	return r
-}
-
-func getall() []string {
-	r := make([]string, 0)
-
-	db.View(func(tx *bolt.Tx) error {
-		b := tx.Bucket([]byte(bucket))
-
-		b.ForEach(func(k, v []byte) error {
-			r = append(r, string(v))
-			return nil
-		})
-
-		return nil
-	})
+func checkids(id string) bool {
+	res, _ := db.FindAll(q.NewQuery(bucket).Where(q.Field("id").Eq(id)))
+	r := true
+	if len(res) == 0 {
+		r = false
+	}
 
 	return r
 }
 
 func gettasks() [][]string {
 	r := make([][]string, 0)
-
-	db.View(func(tx *bolt.Tx) error {
-		b := tx.Bucket([]byte(bucket))
-
-		b.ForEach(func(k, v []byte) error {
-			if string(k) != "l" {
-				r = append(r, []string{string(k), string(v)})
-			}
-			return nil
-		})
-
-		return nil
-	})
-
+	res, _ := db.FindAll(q.NewQuery(bucket))
+	for i := range res {
+		status := res[i].Get("status").(bool)
+		r = append(r, []string{res[i].Get("id").(string), res[i].Get("name").(string), strconv.FormatBool(status)})
+	}
 	return r
 }
 
 func addtask(value string) {
-	db.Update(func(tx *bolt.Tx) error {
-		b := tx.Bucket([]byte(bucket))
-		tkey, _ := strconv.Atoi(string(b.Get([]byte("l"))))
-		tkey++
-		key := strconv.Itoa(tkey)
-		b.Put([]byte(key), []byte(value+"``0"))
-		b.Put([]byte("l"), []byte(key))
-		return nil
-	})
+	task := make(map[string]interface{})
+	task["name"] = value
+	task["status"] = false
+	task["id"] = random()
+	for checkids(task["id"].(string)) { // There is a little to no chance this will ever generate same key twice, but better be safe
+		task["id"] = random()
+	}
+	doc := d.NewDocumentOf(task)
+	db.InsertOne(bucket, doc)
 }
 
 func main() {
@@ -125,19 +101,17 @@ func main() {
 	e.POST("/list", func(c echo.Context) error {
 		user := c.FormValue("user")
 		password := c.FormValue("password")
-		res := "Tasks:\n"
-		r := make([][]string, 0)
 		if user == strings.Trim(auth[0], "\n") && password == strings.Trim(auth[1], "\n") {
-			r = gettasks()
+			var res string
+			r := gettasks()
 			for i := range r {
-				t := strings.Split(r[i][1], "``")
 				var status string
-				if t[1] == "0" {
+				if r[i][2] == "false" {
 					status = "Not finished"
 				} else {
 					status = "Finished"
 				}
-				res += fmt.Sprintf("%v:   %v ----- %v\n", r[i][0], t[0], status)
+				res += fmt.Sprintf("%v:   %v ----- %v\n", r[i][0], r[i][1], status)
 			}
 			return c.String(http.StatusOK, res)
 		}
@@ -150,8 +124,11 @@ func main() {
 		id := c.FormValue("id")
 
 		if user == strings.Trim(auth[0], "\n") && password == strings.Trim(auth[1], "\n") {
-			v := strings.Split(string(get(id)), "``")
-			put(id, v[0]+"``1")
+			update := make(map[string]interface{})
+			update["status"] = true
+
+			que := q.NewQuery(bucket).Where(q.Field("id").Eq(id))
+			db.Update(que, update)
 
 			return c.String(http.StatusOK, "Success!\n")
 		}
